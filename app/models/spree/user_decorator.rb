a596ac65c20548b8aa81f77573dd189fa5e26183
nitesh varma <varmanitesh91@gmail.com>
2019-05-10 00:39:46 +0530

    Updated spree wishlist for Rails 5.2.x

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,5 +1,6 @@
 Spree.user_class.class_eval do
   has_many :wishlists, class_name: Spree::Wishlist.name
+  has_many :wished_products, through: :wishlists
 
   def wishlist
     default_wishlist = wishlists.where(is_default: true).first
@@ -7,5 +8,9 @@ Spree.user_class.class_eval do
     default_wishlist ||= wishlists.create(name: Spree.t(:default_wishlist_name), is_default: true)
     default_wishlist.update_attribute(:is_default, true) unless default_wishlist.is_default?
     default_wishlist
+  end
+
+  def wished_products_count
+    @wished_products_count ||= wished_products.count
   end
 end
